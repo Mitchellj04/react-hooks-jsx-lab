b26057abc704f0bd398042c7b9a012c07c6936c3
JustinMitchell <mitchelljm04@gmail.com>
2022-04-18 18:47:04 -0400

    Assignmnet Completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { image } from "../data/data";
 
+// const divStyle = {
+//   position: center; 
+
+// }
+
 function About() {
-  return <div>About</div>;
+  return (
+    <div id='about'>
+      <h2>About Me</h2>
+      <p>I am a graduate from the Univeristy of Rhode Island where I majored in Entrprenuership and Innovation.
+        Now I am pursuing a career in the computer science field.
+      </p>
+      <img src={image} alt="I made this"></img>
+
+    </div>
+  );
 }
 
 export default About;
